eff8a839aa90516955f0d59b2b2293c3184b774e
Tarass Demcuks <tarass.demcuks@accenture.com>
2023-10-26 22:23:51 +0300

    Site skeleton with placeholders

---
+++ src/app/components/dashboard/dashboard.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mw-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
+})
+export class DashboardComponent {
+
+}
---
+++ src/app/components/details/details.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mw-details',
+  templateUrl: './details.component.html',
+  styleUrls: ['./details.component.css']
+})
+export class DetailsComponent {
+
+}
